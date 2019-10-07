21f7143883bb8d51c4d85b1128d20a7e1fb2ff76
Tomasz Rewak <tomaszrewak@gmail.com>
2019-10-07 21:58:21 +0200

    Adding some new controls.

---
+++ ConsoleMultiplexer/Controls/Grid.cs
@@ -0,0 +1,46 @@
+﻿using ConsoleMultiplexer.Common;
+using ConsoleMultiplexer.Data;
+using ConsoleMultiplexer.Space;
+using ConsoleMultiplexer.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleMultiplexer.Controls
+{
+	public class Grid : Control
+	{
+		struct ColumnDefinition
+		{
+			int Width;
+		}
+
+		struct RowDefinition
+		{
+			int Height;
+		}
+
+		private int _columns;
+		public int Columns
+		{
+			get => _columns;
+			set => Setter
+				.Set(ref _columns, value);
+		}
+
+		private int _rows;
+		public int Rows
+		{
+			get => _rows;
+			set => Setter
+				.Set(ref _rows, value);
+		}
+
+		public override Character this[Position position] => throw new NotImplementedException();
+
+		protected override void Initialize()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
