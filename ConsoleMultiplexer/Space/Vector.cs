8866573e597f1b05f40c65423c9856280f405ba8
Tomasz Rewak <tomaszrewak@gmail.com>
2019-10-21 17:52:51 +0200

    Fixing some warnings.

--- ConsoleMultiplexer/Space/Vector.cs
+++ ConsoleMultiplexer/Space/Vector.cs
@@ -19,5 +19,15 @@ namespace ConsoleMultiplexer.Space
 
 		public static bool operator ==(in Vector lhs, in Vector rhs) => lhs.X == rhs.X && lhs.Y == rhs.Y;
 		public static bool operator !=(in Vector lhs, in Vector rhs) => !(lhs == rhs);
+
+		public override bool Equals(object obj) => obj is Vector vector && this == vector;
+
+		public override int GetHashCode()
+		{
+			var hashCode = 1861411795;
+			hashCode = hashCode * -1521134295 + X.GetHashCode();
+			hashCode = hashCode * -1521134295 + Y.GetHashCode();
+			return hashCode;
+		}
 	}
 }
