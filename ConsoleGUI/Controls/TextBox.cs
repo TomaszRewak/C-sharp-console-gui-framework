674138c31c2afc70e165db76aef51773c3424286
Tomasz Rewak <tomaszrewak@gmail.com>
2019-10-26 21:13:54 +0200

    Making the caret always visible in the text box + adding support for the delete button.

--- ConsoleGUI/Controls/TextBox.cs
+++ ConsoleGUI/Controls/TextBox.cs
@@ -40,20 +40,25 @@ namespace ConsoleGUI.Controls
 
 		private int TextLength => Text?.Length ?? 0;
 		private Size TextSize => new Size(TextLength, 1);
+		private Size EditorSize => CaretEnd >= TextLength ? TextSize.Expand(1, 0) : TextSize;
 
 		public override Character this[Position position]
 		{
 			get
 			{
-				if (!TextSize.Contains(position)) return Character.Empty;
+				if (!EditorSize.Contains(position)) return Character.Empty;
+
+				var character = position.X >= TextLength
+					? (char?)null
+					: Text[position.X];
 
 				if (position.X == CaretStart && position.X == CaretEnd)
-					return new Character(Text[position.X], background: new Color(50, 50, 50));
+					return new Character(character, background: new Color(70, 70, 70));
 
 				if (position.X >= CaretStart && position.X < CaretEnd)
-					return new Character(Text[position.X], background: Color.White, foreground: Color.Black);
+					return new Character(character, background: Color.White, foreground: Color.Black);
 
-				return new Character(Text[position.X]);
+				return new Character(character);
 			}
 		}
 
@@ -66,9 +71,6 @@ namespace ConsoleGUI.Controls
 
 				switch (inputEvent.Key.Key)
 				{
-					case ConsoleKey.UpArrow:
-					case ConsoleKey.DownArrow:
-						return;
 					case ConsoleKey.LeftArrow when inputEvent.Key.Modifiers.HasFlag(ConsoleModifiers.Control):
 						CaretStart = Math.Max(0, CaretStart - 1);
 						break;
@@ -81,6 +83,7 @@ namespace ConsoleGUI.Controls
 					case ConsoleKey.RightArrow:
 						CaretStart = CaretEnd = Math.Min(Text.Length, CaretEnd + 1);
 						break;
+					case ConsoleKey.Delete when CaretStart != CaretEnd:
 					case ConsoleKey.Backspace when CaretStart != CaretEnd:
 						Text = $"{Text.Substring(0, CaretStart)}{Text.Substring(CaretEnd)}";
 						CaretEnd = CaretStart;
@@ -88,6 +91,9 @@ namespace ConsoleGUI.Controls
 					case ConsoleKey.Backspace when CaretStart > 0:
 						Text = $"{Text.Substring(0, CaretStart - 1)}{Text.Substring(CaretStart)}";
 						CaretStart = CaretEnd = CaretStart - 1;
+						break;
+					case ConsoleKey.Delete when CaretStart < TextLength:
+						Text = $"{Text.Substring(0, CaretStart)}{Text.Substring(CaretStart + 1)}";
 						break;
 					case ConsoleKey key when char.IsControl(inputEvent.Key.KeyChar):
 						return;
@@ -100,11 +106,13 @@ namespace ConsoleGUI.Controls
 			}
 
 			inputEvent.Handled = true;
+
+			Initialize();
 		}
 
 		protected override void Initialize()
 		{
-			Resize(Size.Clip(MinSize, TextSize, MaxSize));
+			Resize(EditorSize);
 		}
 	}
 }
