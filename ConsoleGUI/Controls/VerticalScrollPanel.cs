dfdaeabafdbfe6e2c5236d72587e0473235346d8
tofudd <bartlomiej.tota@outlook.com>
2021-04-12 11:55:19 +0200

    Space idents changed to tabs

--- ConsoleGUI/Controls/VerticalScrollPanel.cs
+++ ConsoleGUI/Controls/VerticalScrollPanel.cs
@@ -9,120 +9,120 @@ using System.Text;
 
 namespace ConsoleGUI.Controls
 {
-    public class VerticalScrollPanel : Control, IDrawingContextListener, IInputListener
-    {
-        private DrawingContext _contentContext = DrawingContext.Dummy;
+	public class VerticalScrollPanel : Control, IDrawingContextListener, IInputListener
+	{
+		private DrawingContext _contentContext = DrawingContext.Dummy;
 
-        private DrawingContext ContentContext
-        {
-            get => _contentContext;
-            set => Setter
-                .SetDisposable(ref _contentContext, value)
-                .Then(Initialize);
-        }
+		private DrawingContext ContentContext
+		{
+			get => _contentContext;
+			set => Setter
+				.SetDisposable(ref _contentContext, value)
+				.Then(Initialize);
+		}
 
-        private IControl _content;
+		private IControl _content;
 
-        public IControl Content
-        {
-            get => _content;
-            set => Setter
-                .Set(ref _content, value)
-                .Then(BindContent);
-        }
+		public IControl Content
+		{
+			get => _content;
+			set => Setter
+				.Set(ref _content, value)
+				.Then(BindContent);
+		}
 
-        private int _top;
+		private int _top;
 
-        public int Top
-        {
-            get => _top;
-            set => Setter
-                .Set(ref _top, Math.Min(ContentContext.Size.Height - Size.Height, Math.Max(0, value)))
-                .Then(Initialize);
-        }
+		public int Top
+		{
+			get => _top;
+			set => Setter
+				.Set(ref _top, Math.Min(ContentContext.Size.Height - Size.Height, Math.Max(0, value)))
+				.Then(Initialize);
+		}
 
-        private Character _scrollBarForeground = new Character('▀', foreground: new Color(100, 100, 255));
+		private Character _scrollBarForeground = new Character('▀', foreground: new Color(100, 100, 255));
 
-        public ConsoleKey ScrollUpKey { get; set; } = ConsoleKey.UpArrow;
-        public ConsoleKey ScrollDownKey { get; set; } = ConsoleKey.DownArrow;
-        
-        public Character ScrollBarForeground
-        {
-            get => _scrollBarForeground;
-            set => Setter
-                .Set(ref _scrollBarForeground, value)
-                .Then(RedrawScrollBar);
-        }
+		public ConsoleKey ScrollUpKey { get; set; } = ConsoleKey.UpArrow;
+		public ConsoleKey ScrollDownKey { get; set; } = ConsoleKey.DownArrow;
+		
+		public Character ScrollBarForeground
+		{
+			get => _scrollBarForeground;
+			set => Setter
+				.Set(ref _scrollBarForeground, value)
+				.Then(RedrawScrollBar);
+		}
 
-        private Character _scrollBarBackground = new Character('║', foreground: new Color(100, 100, 100));
+		private Character _scrollBarBackground = new Character('║', foreground: new Color(100, 100, 100));
 
-        public Character ScrollBarBackground
-        {
-            get => _scrollBarBackground;
-            set => Setter
-                .Set(ref _scrollBarBackground, value)
-                .Then(RedrawScrollBar);
-        }
+		public Character ScrollBarBackground
+		{
+			get => _scrollBarBackground;
+			set => Setter
+				.Set(ref _scrollBarBackground, value)
+				.Then(RedrawScrollBar);
+		}
 
-        public override Cell this[Position position]
-        {
-            get
-            {
-                if (position.X != Size.Width - 1)
-                    return ContentContext[position];
+		public override Cell this[Position position]
+		{
+			get
+			{
+				if (position.X != Size.Width - 1)
+					return ContentContext[position];
 
-                if (Content == null) return ScrollBarForeground;
-                if (Content.Size.Height <= Size.Height) return ScrollBarForeground;
-                if (position.Y * Content.Size.Height < Top * Size.Height) return ScrollBarBackground;
-                if (position.Y * Content.Size.Height > (Top + Size.Height) * Size.Height) return ScrollBarBackground;
+				if (Content == null) return ScrollBarForeground;
+				if (Content.Size.Height <= Size.Height) return ScrollBarForeground;
+				if (position.Y * Content.Size.Height < Top * Size.Height) return ScrollBarBackground;
+				if (position.Y * Content.Size.Height > (Top + Size.Height) * Size.Height) return ScrollBarBackground;
 
-                return ScrollBarForeground;
-            }
-        }
+				return ScrollBarForeground;
+			}
+		}
 
-        protected override void Initialize()
-        {
-            using (Freeze())
-            {
-                ContentContext.SetLimits(MaxSize.Shrink(1, 0), MaxSize.Shrink(1, 0).WithInfitineHeight());
-                ContentContext.SetOffset(new Vector(0, -Top));
+		protected override void Initialize()
+		{
+			using (Freeze())
+			{
+				ContentContext.SetLimits(MaxSize.Shrink(1, 0), MaxSize.Shrink(1, 0).WithInfitineHeight());
+				ContentContext.SetOffset(new Vector(0, -Top));
 
-                Resize(Size.Clip(MinSize, ContentContext.Size.Expand(1, 0), MaxSize));
-            }
-        }
+				Resize(Size.Clip(MinSize, ContentContext.Size.Expand(1, 0), MaxSize));
+			}
+		}
 
-        private void BindContent()
-        {
-            ContentContext = new DrawingContext(this, Content);
-        }
+		private void BindContent()
+		{
+			ContentContext = new DrawingContext(this, Content);
+		}
 
-        private void RedrawScrollBar()
-        {
-            Update(Size.WithWidth(1).AsRect().Move(Size.Width - 1, 0));
-        }
+		private void RedrawScrollBar()
+		{
+			Update(Size.WithWidth(1).AsRect().Move(Size.Width - 1, 0));
+		}
 
-        void IDrawingContextListener.OnRedraw(DrawingContext drawingContext)
-        {
-            Initialize();
-        }
+		void IDrawingContextListener.OnRedraw(DrawingContext drawingContext)
+		{
+			Initialize();
+		}
 
-        void IDrawingContextListener.OnUpdate(DrawingContext drawingContext, Rect rect)
-        {
-            Update(rect);
-        }
+		void IDrawingContextListener.OnUpdate(DrawingContext drawingContext, Rect rect)
+		{
+			Update(rect);
+		}
 
-        void IInputListener.OnInput(InputEvent inputEvent)
-        {
-            if (inputEvent.Key.Key == ScrollUpKey)
-            {
-                Top -= 1;
-            }
-            else if (inputEvent.Key.Key == ScrollDownKey)
-            {
-                Top += 1;
-            }
-            
-            inputEvent.Handled = true;
-        }
-    }
+		void IInputListener.OnInput(InputEvent inputEvent)
+		{
+			if (inputEvent.Key.Key == ScrollUpKey)
+			{
+				Top -= 1;
+			}
+			else if (inputEvent.Key.Key == ScrollDownKey)
+			{
+				Top += 1;
+			}
+			
+			inputEvent.Handled = true;
+		}
+	}
 }
