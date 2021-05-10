ae18d6345749b21d9b29cc93fd03176a5177375f
i-am-shodan <6901273+i-am-shodan@users.noreply.github.com>
2021-05-10 12:54:23 +0100

    Code review changes

--- ConsoleGUI/Controls/ListView.cs
+++ ConsoleGUI/Controls/ListView.cs
@@ -4,93 +4,166 @@ using System.Linq;
 using ConsoleGUI.Data;
 using ConsoleGUI.Input;
 using ConsoleGUI.UserDefined;
+using ConsoleGUI.Utils;
 
 namespace ConsoleGUI.Controls
 {
-    public class ListView : SimpleControl, IInputListener
-    {
-        public Color TextColor { get; set; } = Color.White;
-        public Color SelectedTextColor { get; set; } = Color.Black;
+	public class ListView : SimpleControl, IInputListener
+	{
+		private Color _textColor = Color.White;
+		public Color TextColor
+		{
+			get => _textColor;
+			set {
+				if (Setter.Set(ref _textColor, value).Changed)
+				{
+					for (int currentRow = _vscrollp.Top; currentRow < _vscrollp.Size.Height + _vscrollp.Top; currentRow++)
+					{
+						UpdateColor(currentRow, null);
+					}
+					UpdateColor(null, _selectedIndex);
+				}
+			}
+		}
 
-        private int selectedIndex = -1;
-        private int totalCount = -1;
+		private Color _selectedTextColor = Color.Black;
+		public Color SelectedTextColor
+		{
+			get => _selectedTextColor;
+			set
+			{
+				if (Setter.Set(ref _selectedTextColor, value).Changed)
+				{
+					UpdateColor(_selectedIndex, _selectedIndex);
+				}
+			}
+		}
 
-        public ListView()
-        {
-            Content = new VerticalScrollPanel()
-            {
-                Content = new VerticalStackPanel()
-            };
-        }
+		private int ?_selectedIndex = null;
+		public int ?SelectedIndex
+		{
+			get => _selectedIndex;
+			set {
+				var oldVal = _selectedIndex;
+				if (Setter.Set(ref _selectedIndex, value).Changed)
+				{ 
+					UpdateColor(oldVal, value);
+				}
+			}
+		}
 
-        public string SelectedText
-        {
-            get
-            {
-                try
-                {
-                    var selectedItem = ((Content as VerticalScrollPanel).Content as VerticalStackPanel).Children.ElementAt(selectedIndex);
-                    var current = selectedItem as TextBlock;
-                    return current.Text;
-                }
-                catch
-                {
-                    return string.Empty;
-                }
-            }
-        }
+		public ConsoleKey ScrollUpKey => _vscrollp.ScrollUpKey;
+		public ConsoleKey ScrollDownKey => _vscrollp.ScrollDownKey;
 
-        public List<string> Items
-        {
-            set
-            {
-                totalCount = value.Count;
-                selectedIndex = value.Any() ? 0 : -1;
+		private readonly VerticalScrollPanel _vscrollp;
+		private readonly VerticalStackPanel _vstackp;
 
-                var items = new List<TextBlock>();
-                for (int x = 0; x < value.Count; x++)
-                {
-                    items.Add(new TextBlock() { Text = value[x], Color = x == selectedIndex ? SelectedTextColor : TextColor });
-                }
+		public ListView()
+		{
+			Content = new VerticalScrollPanel()
+			{
+				Content = new VerticalStackPanel()
+			};
+			_vscrollp = Content as VerticalScrollPanel;
+			_vstackp = _vscrollp.Content as VerticalStackPanel;
+		}
 
-                ((Content as VerticalScrollPanel).Content as VerticalStackPanel).Children = items;
-            }
-        }
+		public string SelectedItem
+		{
+			get
+			{
+				if (_selectedIndex != null)
+				{
+					var selectedItem = _vstackp.Children.ElementAt(_selectedIndex.Value);
+					var current = selectedItem as TextBlock;
+					return current.Text;
+				}
+				else
+				{
+					return null;
+				}
+			}
+		}
 
-        public void OnInput(InputEvent inputEvent)
-        {
-            if (inputEvent.Key.Key == ConsoleKey.UpArrow)
-            {
-                if (selectedIndex > 0)
-                {
-                    Update(selectedIndex, selectedIndex - 1);
-                    selectedIndex--;
-                }
-            } 
-            else if (inputEvent.Key.Key == ConsoleKey.DownArrow)
-            {
-                if (selectedIndex < totalCount - 1)
-                {
-                    Update(selectedIndex, selectedIndex + 1);
-                    selectedIndex++;
-                }
-            }
+		public IEnumerable<string> Items
+		{
+			set
+			{
+				if (value == null)
+				{
+					_vstackp.Children = new TextBlock[] { };
+					_selectedIndex = null;
+				}
+				else
+				{
+					_selectedIndex = 0;
 
-            // pass through event to ensure scrolling works
-            var input = Content as IInputListener;
-            input.OnInput(inputEvent);
-        }
+					var items = new List<TextBlock>();
+					for (int curIndex = 0; curIndex < value.Count(); curIndex++)
+					{
+						items.Add(new TextBlock()
+						{
+							Text = value.ElementAt(curIndex),
+							Color = curIndex == _selectedIndex ? SelectedTextColor : TextColor
+						});
+					}
 
-        private void Update(int oldRow, int newRow)
-        {
-            var oldElement = ((Content as VerticalScrollPanel).Content as VerticalStackPanel).Children.ElementAt(oldRow);
-            var newElement = ((Content as VerticalScrollPanel).Content as VerticalStackPanel).Children.ElementAt(newRow);
+					_vstackp.Children = items;
+				}
+			}
+			get
+			{
+				return _vstackp.Children.Select(x => x as TextBlock).Select(x => x.Text);
+			}
+		}
 
-            var textBlockOld = oldElement as TextBlock;
-            var textBlockNew = newElement as TextBlock;
+		void IInputListener.OnInput(InputEvent inputEvent)
+		{
+			if (inputEvent.Key.Key == ScrollUpKey)
+			{
+				if (_selectedIndex > 0)
+				{
+					UpdateColor(_selectedIndex, _selectedIndex - 1);
+					_selectedIndex--;
+				}
+			}
+			else if (inputEvent.Key.Key == ScrollDownKey)
+			{
+				if (_selectedIndex < _vstackp.Children.Count() - 1)
+				{
+					UpdateColor(_selectedIndex, _selectedIndex + 1);
+					_selectedIndex++;
+				}
+			}
 
-            textBlockNew.Color = SelectedTextColor;
-            textBlockOld.Color = TextColor;	
-        }
-    }
+			// Page what is displayed based on the selected item
+			var totalNumberOfItemsPossibleOnScreen = _vscrollp.Size.Height;
+			if ((inputEvent.Key.Key == ScrollDownKey && _selectedIndex % totalNumberOfItemsPossibleOnScreen == 0) || (inputEvent.Key.Key == ScrollUpKey && _selectedIndex < _vscrollp.Top))
+			{
+				_vscrollp.Top = inputEvent.Key.Key == ScrollDownKey ? 
+					_vscrollp.Top + totalNumberOfItemsPossibleOnScreen : 
+					_vscrollp.Top - totalNumberOfItemsPossibleOnScreen;
+
+			}
+
+			inputEvent.Handled = true;
+		}
+
+		private void UpdateColor(int ?oldRow, int ?newRow)
+		{
+			if (oldRow != null)
+			{
+				var oldElement = _vstackp.Children.ElementAt(oldRow.Value);
+				var textBlockOld = oldElement as TextBlock;
+				textBlockOld.Color = TextColor;
+			}
+
+			if (newRow != null)
+			{
+				var newElement = _vstackp.Children.ElementAt(newRow.Value);
+				var textBlockNew = newElement as TextBlock;
+				textBlockNew.Color = SelectedTextColor;
+			}		
+		}
+	}
 }
